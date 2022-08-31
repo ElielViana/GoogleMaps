b2a917b53abab1a7d9034cd9d58dd3341808aa2d
Oliver <eliel.viana@estudante.ifto.edu.br>
2022-08-30 21:04:24 -0300

    adds new icon and a counter to the pin icon

--- app/src/main/java/com/example/googlemaps/MapsActivity.java
+++ app/src/main/java/com/example/googlemaps/MapsActivity.java
@@ -1,19 +1,23 @@
 package com.example.googlemaps;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.googlemaps.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
@@ -25,9 +29,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        GoogleMapOptions options = new GoogleMapOptions();
+        options.mapType(GoogleMap.MAP_TYPE_SATELLITE);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance(options);
+        getSupportFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
+
         mapFragment.getMapAsync(this);
     }
 
@@ -46,7 +54,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng ifto = new LatLng(-10.19916, -48.31166);
-        mMap.addMarker(new MarkerOptions().position(ifto).title("IFTO - Instituto Federal do Tocantins")).setIcon(BitmapDescriptorFactory.fromResource(R.drawable.pino_de_localizacao__1_));
+        mMap.addMarker(new MarkerOptions()
+                        .position(ifto)
+                        .title("IFTO - Instituto Federal do Tocantins - Palmas"))
+                .setIcon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.pino_de_localizacao__2_));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(ifto));
+
+        LatLng iftoArg = new LatLng(-7.1814258, -48.1959622);
+        mMap.addMarker(
+                new MarkerOptions()
+                        .position(iftoArg)
+                        .title("Araguaína")
+                        .draggable(true)
+                        .snippet("IFTO - Instituto Federal do Tocantins - Araguaína")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))).setTag(0);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iftoArg, 5));
+        mMap.setMinZoomPreference(5);
+        mMap.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        Integer count = (Integer) marker.getTag();
+        if (count != null) {
+            count++;
+            marker.setTag(count);
+            marker.setTitle("Marcador clicado " + count + " vezes");
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
